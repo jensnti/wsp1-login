8d5686b03137c7436b06510cab3e03537286794a
jensnti <jens.andreasson@ga.ntig.se>
2021-02-06 11:01:17 +0100

    feat: bcrypt package
    feat(login.js): adds code to has pwd and uses bcrypt for hasing pwd

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const { query } = require('../models/db');
 
@@ -8,7 +9,7 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
-// for action
+/* Try to login user */
 router.post('/', async function(req, res, next) {
   const username = req.body.username;
   const password = req.body.password;
@@ -16,22 +17,42 @@ router.post('/', async function(req, res, next) {
   if (username && password) {
     // check if user exists
     try {
-      const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+      const sql = 'SELECT password FROM users WHERE username = ?';
 
       const result = await query(sql, username, password);
-
-      if (result.length > 0) {
-        req.session.loggedin = true;
-        req.session.username = username;
-        res.redirect('/dashboard');
-      } else {
-        res.send('Incorrect Username and/or Password!');
-      }
+      
+      bcrypt.compare(password, result[0].password, function(err, result) {
+        if (result == true) {
+          req.session.loggedin = true;
+          req.session.username = username;
+          res.redirect('/dashboard');
+        } else {
+          res.send('Incorrect Username and/or Password!');
+        }
+      });
     } catch(e) {
       next(e);
       console.error(e);
     }
   }
+});
+
+/* https://www.npmjs.com/package/bcrypt
+ * En route för att skapa en password has eftersom vi inte har en
+ * registrering
+ * När denna hash är sparad i databasen behöver inloggnings
+ */ 
+
+router.get('/hash/:pwd', async function(req, res, next) {
+  const password = req.params.pwd;
+
+  bcrypt.hash(password, 10, function(err, hash) {
+      console.log(hash);
+      res.status(200);
+      res.json({
+        pwd: hash
+      });
+  });
 });
 
 module.exports = router;
